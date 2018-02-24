1553b4142ec2c525e597dc62834704435e5bf90e
alex zhang <gzane24@gmail.com>
2018-02-24 21:27:28 +0800

    egg-normal-response

---
+++ app/extend/context.js
@@ -0,0 +1,75 @@
+/**
+ * Created by alex on 2017/9/16.
+ */
+
+'use strict';
+
+const SUCCESS_RESPCODE = 'S0001';
+
+module.exports = {
+
+  findRespMessage(respCode) {
+    const respCfg = this.app.config.normalResponse.CODES;
+    if (!respCfg) return;
+
+    const respCodeItem = respCfg[respCode];
+    if (!respCodeItem) return;
+
+    return respCodeItem.message;
+  },
+
+  findRespStatus(respCode) {
+    const respCfg = this.app.config.normalResponse.CODES;
+    if (!respCfg) return;
+
+    const respCodeItem = respCfg[respCode];
+    if (!respCodeItem) return;
+
+    return respCodeItem.status;
+  },
+
+  formatSuccessResp({data, msg, status}) {
+    const respCode = SUCCESS_RESPCODE;
+    const respMessage = msg || this.findRespMessage(respCode) || '成功';
+
+    this.body = {
+      respCode,
+      data,
+      respMessage,
+    };
+
+    if (status) {
+      this.status = status;
+    }
+  },
+
+  formatFailResp({errCode, respErrorDetails, msg, status}) {
+
+    errCode = errCode || 'F500';
+    respErrorDetails = this.app.config.normalResponse.respErrorDetails ? respErrorDetails : undefined;
+    const respMessage = msg || this.findRespMessage(errCode) || '服务异常';
+    this.body = {
+      respCode: errCode,
+      respMessage,
+      respErrorDetails,
+    };
+
+    status = status || this.findRespStatus(errCode) || 500;// 默认
+
+    if (status) {
+      this.status = status;
+    }
+  },
+
+  isSuccessResp() {
+    return this.body && this.body.respCode === SUCCESS_RESPCODE;
+  },
+
+  isFailResp() {
+    return this.status >= 400 || (this.body && this.body.respCode !== SUCCESS_RESPCODE);
+  },
+
+  isNormalResp() {
+    return this.body && this.body.hasOwnProperty('respCode');
+  },
+};
--- config/config.default.js
+++ config/config.default.js
@@ -5,6 +5,45 @@
  * @member Config#normalResponse
  * @property {String} SOME_KEY - some description
  */
-exports.normalResponse = {
 
+const CODES = {
+  S0001: {
+    status: 200,
+    msg: '成功',
+  },
+
+  F400: {
+    status: 400,
+    message: '参数错误'
+  },
+
+  F401: {
+    status: 401,
+    message: '未授权'
+  },
+
+  F403: {
+    status: 403,
+    message: '禁止访问'
+  },
+
+  F404: {
+    status: 404,
+    message: '资源不存在'
+  },
+
+  F429: {
+    status: 429,
+    message: '频繁请求，请稍后尝试'
+  },
+
+  F500: {
+    status: 500,
+    message: '服务异常'
+  }
+};
+
+exports.normalResponse = {
+  CODES,
+  respErrorDetails: false,
 };
